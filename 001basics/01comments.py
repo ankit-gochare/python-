e4ddb72fced7ad102e1a4c63121ef0f2b81fc1f9
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-03 12:45:27 +0530

    comments in python

---
+++ 001basics/01comments.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+
+# single line comment
+# My name is Ankit 
+
+# multi line comments
+"""
+Hello Everyone 
+My name is Ankit
+And I am Leraning python 
+"""
+
+
+# notes
+"""
+Comments
+single line comment #
+#this is a comment
+
+MultiLine comments “”” ”””
+“””hello this is multi
+line comments “””
+
+"""
