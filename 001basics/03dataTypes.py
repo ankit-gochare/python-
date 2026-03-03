d16531222fca9ccfb8a2dfd06e2ff473b1917060
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-03 12:46:26 +0530

    data types in puthon

---
+++ 001basics/03dataTypes.py
@@ -0,0 +1,64 @@
+# Number
+a = 12
+print(type(a)) # <class 'int'>
+
+b = 12.3
+print(type(b)) # <class 'float'>
+
+c = 12/3
+print(type(c)) # <class 'float'>
+
+d = 23j
+print(type(d)) # <class 'complex'>
+
+e = 23.5j
+print(type(e)) # <class 'complex'>
+
+f = 23/5j
+print(type(f)) # <class 'complex'>
+
+
+# String str
+g = "girl"
+print(type(g)) # <class 'str'>
+
+h = 'home'
+print(type(h)) # <class 'str'>
+
+j = 'he said "python is on"' 
+print(type(j)) # <class 'str'>
+
+i = "Love her"
+print(type(i)) # <class 'str'>
+
+# Boolean
+u = True 
+print(type(u)) # <class 'bool'>
+
+me = False
+print(type(me)) # <class 'bool'>
+
+
+
+
+
+# notes
+"""
+Data types
+
+There are major 3 types of data in python
+
+1)Number - int(1 , 2 , 3...), float(12.34 , 67.90) , complex(2j , 3j)
+2)String - use to store anything in "" or ''
+3)Boolean - True and False
+
+Integers are numbers (negative infinity... -3 ,-2 ,-1 ,0, 1, 2 ... to positive infinity)
+
+Float are number which have numbers with decimal point and fraction (p/q) value.
+
+Complex are the numbers with imaginary values
+
+Strings can contain any values, characters and everything
+
+Boolean will only return True and False
+"""
