aa9a81955ab9651a5e88946eb10896d3479b827e
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-03 12:45:59 +0530

    Varibales in python

---
+++ 001basics/02variables.py
@@ -0,0 +1,41 @@
+a = 12
+b = 56
+chacha = 45
+papa = 67
+
+# camelCase
+fullName = "Ankit Gochare"
+
+# PascalCase
+FullName = "Ankit Gochare"
+
+# snake_case
+full_name = "Ankit Gochare"
+# we prefer snake case in python
+
+
+
+# notes
+"""
+Understanding the need of variables
+
+Variables
+
+Variables are used to store something in python
+Variable can be initialized when we assign value to it
+eg -
+a = 12
+b = 67
+
+
+Naming conventions in variable
+3 types -
+camel case - helloWorld
+pascal case - HelloWorld
+Snake case - hello_world
+
+We can use any type in python
+You cannot start a variable with a number
+You cannot use spaces in variable
+You cannot use special characters in variable exception (_)
+"""
